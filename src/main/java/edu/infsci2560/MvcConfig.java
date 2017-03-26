f77f41826232f4b87c44fbdeff6b77d10c70a3f8
Neil <wuyuedehuanghun@gmail.com>
2017-03-26 13:04:07 -0400

    debug 16

--- src/main/java/edu/infsci2560/MvcConfig.java
+++ src/main/java/edu/infsci2560/MvcConfig.java
@@ -10,7 +10,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("land");
+        registry.addViewController("/").setViewName("home");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/nav").setViewName("nav");
         registry.addViewController("/rooms").setViewName("rooms");
